4293668fc800e02324cecc45a7ed8f5992fb355a
ashprice <eisleggje@gmail.com>
2024-01-17 13:25:36 +0000

    Adding initial files

---
+++ README.md
@@ -0,0 +1,35 @@
+# fcitx5-ipa-sil
+
+This is an IME for [fcitx5](https://fcitx-im.org/wiki/Fcitx_5) that brings support for the [SIL IPA](https://keyman.com/keyboards/sil_ipa) input method.
+
+This allows the user to type IPA characters fairly easily by inputting only ASCII.
+
+Here are a few examples of input to IPA conversion:
+
+| IPA | input |
+|-----|-------|
+| ə   | `e=`  |
+| ʃ   | `s=`  |
+| ŋ   | `n>`  |
+| a   | `a`   |
+
+**This is still a work in progress**: I forked [yukijou](https://git.kemonomimi.gay/yukijoou)'s original layout to add support for more characters and diacritic combinations. I haven't looked into how to support stacked diacritics yet, so if you want to give a pull request or start your own fork, please let me know! (I will probably package this for the Arch user repository soon.)
+
+## Building & trying it out!
+
+```console
+$ mkdir -p build/
+$ cd build/
+$ cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
+$ make
+$ sudo make install
+```
+
+You can then restart fcitx5, and add the `SIL IPA` input method to your selected input methods.
+
+
+# Credits
+
+Original development by [yukijou](https://git.kemonomimi.gay/yukijoou). I have created a fork of their original layout so that I can customize it and make my own - I wasn't aware of how to write fcitx5 layouts before looking at theirs, and will probably write some more in addition to this one, but all original credit for the basic code goes to them.
+
+
---
+++ src/ipa-sil.cpp
@@ -0,0 +1,165 @@
+/*
+	 * SPDX-FileCopyrightText: 2023 Yuki Joou <yukijoou [a] kemonomimi [-] gay>
+	 *
+	 * SPDX-License-Identifier: BSD-3-Clause
+	 *
+	 */
+	#include "ipa-sil.hpp"
+	
+	#include <fcitx/inputpanel.h>
+	#include <fcitx/instance.h>
+	
+	std::optional<std::string> getIPAForSIL(std::string   baseCharacter,
+	                                        fcitx::KeySym silModifier)
+	{
+		// TODO: Use a proper data structure, like a hash map
+		//       Even better: load that data off of a file!
+	
+		// TODO: Add more characters! This is only enough for narrow RP
+		//       transcriptions...
+		if (baseCharacter == "s") {
+			if (silModifier == FcitxKey_equal) return "ʃ";
+		}
+	
+		if (baseCharacter == "z") {
+			if (silModifier == FcitxKey_equal) return "ʒ";
+		}
+	
+		if (baseCharacter == "t") {
+			if (silModifier == FcitxKey_equal) return "θ";
+		}
+	
+		if (baseCharacter == "d") {
+			if (silModifier == FcitxKey_equal) return "ð";
+		}
+	
+		if (baseCharacter == "n") {
+			if (silModifier == FcitxKey_greater) return "ŋ";
+		}
+	
+		if (baseCharacter == "i") {
+		if (silModifier == FcitxKey_equal) return "ɪ";
+		}
+	
+		if (baseCharacter == "e") {
+			if (silModifier == FcitxKey_equal) return "ə";
+		}
+	
+		if (baseCharacter == "a") {
+			if (silModifier == FcitxKey_less) return "æ";
+		if (silModifier == FcitxKey_equal) return "ɑ";
+		}
+	
+		if (baseCharacter == "o") {
+			if (silModifier == FcitxKey_less) return "ɔ";
+			if (silModifier == FcitxKey_equal) return "ɒ";
+		}
+	
+		if (baseCharacter == "u") {
+			if (silModifier == FcitxKey_greater) return "ʌ";
+			if (silModifier == FcitxKey_less) return "ʊ";
+		}
+	
+		return {};
+	}
+	
+	bool isSILModifier(fcitx::KeySym key)
+	{
+		return key == FcitxKey_less || key == FcitxKey_greater ||
+		       key == FcitxKey_equal;
+	}
+	
+	void SILState::handleAlphaKey(fcitx::Key key)
+	{
+		// TODO: Don't make this a special case, have a better system for handling
+		//       this!
+		if (key.check(FcitxKey_colon)) {
+		m_buffer.type("ː");
+			m_lastKey.reset();
+			return;
+		}
+		
+		if (!isSILModifier(key.sym()) || !m_lastKey.has_value()) {
+			m_lastKey = key.toString();
+			m_buffer.type(key.sym());
+			return;
+		}
+	
+		if (isSILModifier(key.sym())) {
+			auto ipaChar = getIPAForSIL(*m_lastKey, key.sym());
+			if (ipaChar) {
+				m_buffer.backspace();
+				m_buffer.type(*ipaChar);
+			}
+			m_lastKey.reset();
+		}
+	}
+	
+	void SILState::keyEvent(fcitx::KeyEvent& keyEvent)
+	{
+		// If it's not a simple letter key, and we have no text input yet, let it
+		// through.
+		if (m_buffer.empty() && !keyEvent.key().isSimple()) return;
+	
+		if (keyEvent.key().check(FcitxKey_Return)) {
+			m_ic->commitString(m_buffer.userInput());
+			reset();
+		} else if (keyEvent.key().check(FcitxKey_space)) {
+			m_ic->commitString(m_buffer.userInput());
+			m_ic->commitString(" ");
+			reset();
+		} else if (keyEvent.key().check(FcitxKey_BackSpace))
+			m_buffer.backspace();
+		else if (keyEvent.key().isSimple())
+			handleAlphaKey(keyEvent.key());
+		//	m_buffer.type(keyEvent.key().sym());
+		updateUI();
+	
+		keyEvent.filterAndAccept();
+	}
+	
+	void SILState::updateUI()
+	{
+		auto& inputPanel = m_ic->inputPanel();
+		inputPanel.reset();
+	
+		if (m_ic->capabilityFlags().test(fcitx::CapabilityFlag::Preedit)) {
+			fcitx::Text preedit(m_buffer.userInput(),
+			                    fcitx::TextFormatFlag::HighLight);
+			inputPanel.setClientPreedit(preedit);
+		} else {
+			fcitx::Text preedit(m_buffer.userInput());
+			inputPanel.setPreedit(preedit);
+		}
+		m_ic->updateUserInterface(fcitx::UserInterfaceComponent::InputPanel);
+		m_ic->updatePreedit();
+	}
+	
+	SILEngine::SILEngine(fcitx::Instance* instance)
+	    : m_factory(
+	          [this](fcitx::InputContext& ic) { return new SILState(this, &ic); })
+	{
+		instance->inputContextManager().registerProperty("silState", &m_factory);
+	}
+	
+	void SILEngine::keyEvent(const fcitx::InputMethodEntry&,
+	                         fcitx::KeyEvent& keyEvent)
+	{
+		if (keyEvent.isRelease() || keyEvent.key().states()) {
+			return;
+		}
+	
+		auto  ic    = keyEvent.inputContext();
+		auto* state = ic->propertyFor(&m_factory);
+		state->keyEvent(keyEvent);
+	}
+	
+	void SILEngine::reset(const fcitx::InputMethodEntry&,
+	                      fcitx::InputContextEvent& event)
+	{
+		auto* state = event.inputContext()->propertyFor(&m_factory);
+		state->reset();
+	}
+	
+	FCITX_ADDON_FACTORY(SILEngineFactory);
+
